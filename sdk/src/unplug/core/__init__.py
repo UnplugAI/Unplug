f05a63e8a7c09c2c12072bf30c90d00db372f4ad
chiruu12 <chirag.gupta.290403@gmail.com>
2026-06-12 14:23:04 +0530

    fix core init import order

--- sdk/src/unplug/core/__init__.py
+++ sdk/src/unplug/core/__init__.py
@@ -3,15 +3,15 @@
 from __future__ import annotations
 
 from unplug.core.context import ExecutionContext, ToolCall
-from unplug.core.models import ModelProvider, ModelRegistry, ModelSpec
-from unplug.core.secrets import SecretsRegistry, SecretsSanitizer
-from unplug.core.stats import MetricsCollector
 from unplug.core.disposition import (
     DispositionLabel,
     DispositionPrediction,
     DualHeadWithDisposition,
     resolve_disposition,
 )
+from unplug.core.models import ModelProvider, ModelRegistry, ModelSpec
+from unplug.core.secrets import SecretsRegistry, SecretsSanitizer
+from unplug.core.stats import MetricsCollector
 from unplug.core.taint import Tagger, TaintedText, TrustLevel
 
 __all__ = [
